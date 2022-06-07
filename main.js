e68303a231c6094d1b1b5e29140d190809d00076
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-07 11:54:30 -0400

    Removing the laser and the invader when i shoot the laser and it contains an invader

--- main.js
+++ main.js
@@ -105,6 +105,12 @@ const shoot = (e) => {
     squares[currentLaserIndex].classList.remove('laser')
     currentLaserIndex -= width
     squares[currentLaserIndex].classList.add('laser')
+    if (squares[currentLaserIndex].classList.contains('invader')) {
+      squares[currentLaserIndex].classList.remove('laser')
+      squares[currentLaserIndex].classList.remove('invader')
+      squares[currentLaserIndex].classList.add('boom')
+
+    }
   }
   switch (e.key) {
     case "ArrowUp":
